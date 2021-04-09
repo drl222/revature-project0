49ca06d1c6039ac056e1466a78cbea3eb12c7bb1
Derrick Lin <drl222@cornell.edu>
2021-04-09 04:33:08 -0400

    Implemented a safari system

--- project0/src/main/scala/Cli.scala
+++ project0/src/main/scala/Cli.scala
@@ -6,7 +6,7 @@ import scala.util.Random
 
 class Cli {
   val parse_RE: Regex = raw"(\w+)\s*(.*)".r
-  val pokedex_file_location: String = "pokedex2.csv"
+  val pokedex_file_location: String = "pokedex.csv"
   var pokedex: Vector[Pokemon] = null
   val rng: Random = new Random()
 
@@ -17,19 +17,74 @@ class Cli {
     var do_exit: Boolean = !load_successful
 
     while (!do_exit) {
+      println("You're standing in the Pokémon Safari Welcome Center.")
+      println("What would you like to do?")
       print(">> ")
       var input = StdIn.readLine()
       parse(input) match {
-        case Some(("catch", "")) => println(get_random_pokemon())
-        case Some(("catch", a)) => println(get_random_pokemon(_.type_1.getOrElse("") == a))
+        case Some(("safari", "")) => println(); run_safari("General")
+        // case Some(("catch", "")) => println(get_random_pokemon())
+        // case Some(("catch", a)) => println(get_random_pokemon(_.type_1.getOrElse("") == a))
         case Some(("exit", _)) => {
           println("Goodbye!")
           do_exit = true
         }
-        case Some((cmd, arg)) =>
-          println(s"""command "$cmd" not understood with arguments "$arg"""")
-        case None => println("command unable to be parsed")
+        case Some((_, _)) | None =>
+          println("command not understood")
       }
+      println()
+    }
+  }
+
+  private def run_safari(
+      safari_name: String,
+      predicate: Pokemon => Boolean = _ => true
+  ): Unit = {
+    var do_exit: Boolean = false
+    println(s"Welcome to the $safari_name Pokémon Safari!")
+    var pkmn: Pokemon = null
+    get_random_pokemon(predicate) match {
+      case Some(pokemon) => {
+        pkmn = pokemon
+      }
+      case None => {
+        println(
+          "Hmm... you can't seem to find any Pokémon in this safari. Returning..."
+        )
+        return ()
+      }
+    }
+    while (!do_exit) {
+      println(s"You spot a ${pkmn.name}! What would [you] like to do?")
+      println(s"You have [infinite] Pokéballs left.")
+
+      print(">> ")
+      var input = StdIn.readLine()
+      parse(input) match {
+        case Some(("catch", _)) => {
+          println(s"You caught a ${pkmn.name}!")
+          //because we've already run it once, we can be confident get_random_pokemon isn't None
+          pkmn = get_random_pokemon(
+            predicate
+          ).get
+        }
+        case Some(("flee", _)) => {
+          println(s"You ignore the ${pkmn.name} and continue searching")
+          //because we've already run it once, we can be confident get_random_pokemon isn't None
+          pkmn = get_random_pokemon(
+            predicate
+          ).get
+        }
+        case Some(("leave", _)) => {
+          println(
+            s"You decide you've explored enough in the $safari_name Pokémon Safari. You head back to the Welcome Center."
+          )
+          do_exit = true
+        }
+        case Some((_, _)) | None =>
+          println("command not understood")
+      }
+      println()
     }
   }
 
@@ -72,7 +127,9 @@ class Cli {
       predicate: Pokemon => Boolean = _ => true
   ): Option[Pokemon] = {
     val filtered_pokedex = pokedex.filter(predicate)
-    var cumulative_weights: Double = filtered_pokedex.foldLeft(0.0)({_ + _.catch_rate.getOrElse(0.0)})
+    var cumulative_weights: Double = filtered_pokedex.foldLeft(0.0)({
+      _ + _.catch_rate.getOrElse(0.0)
+    })
 
     //This is O(n) - maybe inefficient because you can probably do some fancy binary search, but the .filter above is already O(n)
     for (pkmn <- filtered_pokedex) {
